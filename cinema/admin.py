bdcd8119a564168015558119a32a64c361e4eedf
LeonNapoleonov <142759206+ldolgopolov@users.noreply.github.com>
2024-09-23 22:55:24 +0300

    An admin panel was created
    An admin panel was created to edit data in the database.

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Movie, Session, Employee, User, Ticket
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'duration')
+    search_fields = ('title',)
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'start_time', 'end_time')
+    search_fields = ('movie__title',)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'work_position', 'age', 'hourly_salary')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'session', 'expiration_time')
+    search_fields = ('session__movie__title',)
